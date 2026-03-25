7b4531f59e2bb7a55c960e671a1d723796535087
MARIA EDUARDA ZABEL <maria_e_zabel@estudante.sesisenai.org.br>
2026-03-25 15:26:18 -0300

    MZ feat: implementa nova tela, e organiza paleta de cores

--- app/src/main/java/com/example/api_receitas/features/authentication/ui/AuthenticationScreen.kt
+++ app/src/main/java/com/example/api_receitas/features/authentication/ui/AuthenticationScreen.kt
@@ -1,12 +1,34 @@
 package com.example.api_receitas.features.authentication.ui
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.PointerIcon.Companion.Text
+
 
 class AuthenticationScreen {
 
-    @Composable
-    fun AuthenticationScreen(){
+    data class Usuario(
+        val email: String,
+        val senha: String
+    )
 
+    @Composable
+    fun AuthenticationScreenLogIn(modifier: Modifier = Modifier){
+        Column {
+            Text(text = "Log In")
+            Text(text = "Faça Log In a uma conta existente!")
+        }
+
+        Column {
+            var email by remember { mutableStateOf("") }
+            var senha by remember { mutableStateOf("") }
+        }
     }
 
 }
--- app/src/main/java/com/example/api_receitas/ui/theme/Color.kt
+++ app/src/main/java/com/example/api_receitas/ui/theme/Color.kt
@@ -2,6 +2,14 @@ package com.example.api_receitas.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
 val AzulEscuro = Color(0xFF2C4E8D)
 val AzulEnvelhicido = Color(0xFF7C91B8)
 val AzulClaro = Color(0xFFB1CCFF)
