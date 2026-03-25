6f6a8d7934771076be3dea91456121892de381c6
ELIS JASPER <elis_jasper@estudante.sesisenai.org.br>
2026-03-25 15:34:32 -0300

    feat/adicionar bottom bar

--- app/src/main/java/com/example/api_receitas/MainActivity.kt
+++ app/src/main/java/com/example/api_receitas/MainActivity.kt
@@ -3,6 +3,10 @@ package com.example.api_receitas
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.example.api_receitas.features.home.ui.HomeScreen
 import com.example.api_receitas.ui.theme.APIReceitasTheme
 
 class MainActivity : ComponentActivity() {
@@ -10,7 +14,11 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             APIReceitasTheme {
-
+                Surface(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    HomeScreen()
+                }
             }
         }
     }
--- app/src/main/java/com/example/api_receitas/features/home/ui/HomeScreen.kt
+++ app/src/main/java/com/example/api_receitas/features/home/ui/HomeScreen.kt
@@ -1,4 +1,81 @@
 package com.example.api_receitas.features.home.ui
 
-class HomeScreen {
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.api_receitas.R
+
+@Composable
+fun HomeScreen(){
+    Scaffold(
+        bottomBar = { BottomNavBar() }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+        ) {
+            Text(text = "Minhas receitas")
+        }
+    }
+}
+
+@Composable
+fun BottomNavBar(){
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        shape = RoundedCornerShape(32.dp),
+        color = Color.White,
+        shadowElevation = 8.dp
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp, horizontal = 24.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(
+                    painter = painterResource(R.drawable.book),
+                    contentDescription = "Tela incial",
+                    modifier = Modifier.size(28.dp)
+                )
+            }
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(
+                    painter = painterResource(R.drawable.plus),
+                    contentDescription = "Adicionar receita",
+                    modifier = Modifier.size(28.dp)
+                )
+            }
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(
+                    painter = painterResource(R.drawable.search),
+                    contentDescription = "Pesquisar receita",
+                    modifier = Modifier.size(28.dp)
+                )
+            }
+        }
+    }
 }
